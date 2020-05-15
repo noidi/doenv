147d0718ef64861419877feb3c83aca508da119a
Timo Mihaljov <timo@mihaljov.info>
2020-05-15 15:28:16 +0300

    Rename ENV -> TOKEN_NAME

--- doenv_core.sh
+++ doenv_core.sh
@@ -6,18 +6,18 @@ read -r password <"$1"
 shift
 
 if [[ $# -lt 2 ]]; then
-  echo -e "Usage: doenv ENV COMMAND [ARG]...\n" >&2
+  echo -e "Usage: doenv TOKEN_NAME COMMAND [ARG]...\n" >&2
   exit 1
 fi
 
-env=$1
+token_name=$1
 shift
 
 doenv_dir="$HOME/.doenv"
 token_dir="$doenv_dir/tokens"
 mkdir -p "$token_dir"
 
-token_file="$token_dir/$env"
+token_file="$token_dir/$token_name"
 if [[ -f $token_file ]]; then
   token=$(gpg --batch \
               --quiet \
@@ -26,7 +26,8 @@ if [[ -f $token_file ]]; then
               --pinentry-mode loopback \
               "$token_file")
 else
-  read -r -s -p "Access token for new environment '$env': " token
+  echo "Creating a new access token: $token_name"
+  read -r -s -p "Copy and paste the token here (input will be hidden): " token
   echo
   gpg --batch \
       --quiet \
