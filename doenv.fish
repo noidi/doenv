f2319ab589ccc6b905a5dffaec744460f1fe0bf2
Timo Mihaljov <timo@mihaljov.info>
2020-05-15 14:27:54 +0300

    Add the Fish version

---
+++ doenv.fish
@@ -0,0 +1,13 @@
+set __doenv_dir (cd (dirname (status --current-filename)); and pwd)
+
+function doenv
+    if not set -q __doenv_password
+        read -g -s -P 'doenv password: ' __doenv_password
+        functions --copy fish_prompt fish_prompt_old
+        function fish_prompt
+            echo -n 'DO '
+            fish_prompt_old
+        end
+    end
+    bash "$__doenv_dir/doenv_core.sh" (echo $__doenv_password | psub) $argv
+end
